14f02452b29eabd39475c3b2986a19aa056d2413
inytar <pietpiet@fastmail.net>
2017-01-25 16:34:17 -0500

    Remove local bower folder and edit dependencies

--- abstract_salad_bar/static.py
+++ abstract_salad_bar/static.py
@@ -5,20 +5,16 @@ from .app import App
 
 bower = bowerstatic.Bower()
 
-
 components = bower.components(
     'components',
     bowerstatic.module_relative_path('bower_components')
 )
 
 
-local = bower.local_components('local', components)
 
 
-local.component(bowerstatic.module_relative_path('local_component'),
-                version=None)
 
 
 @App.static_components()
 def get_static_components():
-    return local
+    return components
