9f898169d7189e8fff0c33bd90f43a2313aa1034
inytar <pietpiet@fastmail.net>
2017-01-31 16:23:36 -0500

    Use pbr in setup.py

--- setup.py
+++ setup.py
@@ -1,44 +1,9 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
-
-name = 'abstract_salad_bar'
-description = (
-    'A reimplementation of the abstract salad bar app.'
-)
-version = '0.0.0'
-
+from setuptools import setup
 
 setup(
-    name=name,
-    version=version,
-    description=description,
-    author='Pieter',
-    author_email='pietpiet@fastmail.net',
-    packages=find_packages(),
-    namespace_packages=name.split('.')[:-1],
-    include_package_data=True,
-    zip_safe=False,
-    platforms='any',
-    install_requires=[
-        'morepath'
-    ],
-    extras_require=dict(
-        test=[
-            'pytest',
-            'webtest',
-        ],
-    ),
-    entry_points=dict(
-        console_scripts=[
-            'run-app = abstract_salad_bar.__main__:run',
-        ],
-    ),
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Environment :: Web Environment',
-        'Topic :: Internet :: WWW/HTTP :: WSGI',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-    ]
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
